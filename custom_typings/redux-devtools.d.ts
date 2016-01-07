dff9a9c83534267ae606db8caf09bc9ed167eaa2
Adam Burmister <adam.burmister@pinterest.com>
2016-01-07 13:36:12 -0800

    Refine type

--- custom_typings/redux-devtools.d.ts
+++ custom_typings/redux-devtools.d.ts
@@ -1,14 +1,15 @@
+/// <reference path="../typings/react/react.d.ts" />
+/// <reference path="../typings/redux/redux.d.ts" />
+
 // Type definitions for redux-devtools 3.0.0
 declare module 'redux-devtools' {
-  export function createDevTools(monitor: any): React.Component<any, any>;
+  export function createDevTools(monitor: any);
 }
 
 declare module 'redux-devtools-log-monitor' {
-  export default class LogMonitor extends React.Component<any, any> {
-  }
+  export default class LogMonitor extends React.Component<any, any> { }
 }
 
 declare module 'redux-devtools-dock-monitor' {
-  export default class DocMonitor extends React.Component<any, any> {
-  }
+  export default class DocMonitor extends React.Component<any, any> { }
 }
