487e4a985e4ba77bd5518313aff150f6db5903d8
Adam Burmister <adam.burmister@pinterest.com>
2016-01-07 11:27:28 -0800

    Revert guard on react-hmre injection

--- build/webpack-environments/development.js
+++ build/webpack-environments/development.js
@@ -30,13 +30,14 @@ export default (webpackConfig) => {
     )
 
     webpackConfig.eslint.emitWarning = true
+    webpackConfig.tslint.emitWarning = true
 
     // We need to apply the react-transform HMR plugin to the Babel configuration,
     // but _only_ when HMR is enabled. Putting this in the default development
     // configuration will break other tasks such as test:unit because Webpack
     // HMR is not enabled there, and these transforms require it.
     webpackConfig.module.loaders = webpackConfig.module.loaders.map(loader => {
-      if (/babel/.test(loader.loader) && loader.query && !~loader.query.presets.indexOf('react-hmre')) {
+      if (/babel/.test(loader.loader) && !~loader.query.presets.indexOf('react-hmre')) {
         debug('Apply react-transform-hmre preset.')
         loader.query.presets.push('react-hmre')
       }
